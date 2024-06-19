facfbe4d08f3162cd942b389e0bb2a7f24f9355f
Joao Henrique <joaoadsistemas@gmail.com>
2024-06-19 15:33:28 -0300

    finished

---
+++ livro_de_ofertas/livro_de_ofertas/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Globalization;
+
+public class Program
+{
+    public static void Main()
+    {
+        int numNotificacoes = int.Parse(Console.ReadLine());
+        Livro[] livroDeOfertas = new Livro[0];
+
+        for (int i = 0; i < numNotificacoes; i++)
+        {
+            string[] notificacao = Console.ReadLine().Split(',');
+            int posicao = int.Parse(notificacao[0]);
+            int acao = int.Parse(notificacao[1]);
+            double valor = double.Parse(notificacao[2]);
+            int quantidade = int.Parse(notificacao[3]);
+
+            livroDeOfertas = ProcessarNotificacao(livroDeOfertas, posicao, acao, valor, quantidade);
+        }
+
+        ImprimirLivroDeOfertas(livroDeOfertas);
+    }
+
+    static Livro[] ProcessarNotificacao(Livro[] livroDeOfertas, int posicao, int acao, double valor, int quantidade)
+    {
+        if (acao == 0)
+        {
+            return InserirOferta(livroDeOfertas, posicao, valor, quantidade);
+        }
+        else if (acao == 1)
+        {
+            return ModificarOferta(livroDeOfertas, posicao, valor, quantidade);
+        }
+        else if (acao == 2)
+        {
+            return RemoverOferta(livroDeOfertas, posicao);
+        }
+        else
+        {
+            Console.WriteLine("Ação inválida.");
+            return livroDeOfertas;
+        }
+
+    }
+
+    static Livro[] InserirOferta(Livro[] livroDeOfertas, int posicao, double valor, int quantidade)
+    {
+        if (posicao != 1 && posicao == livroDeOfertas[livroDeOfertas.Length - 1].Posicao)
+        {
+            ModificarOferta(livroDeOfertas, posicao, valor, quantidade);
+            return livroDeOfertas;
+        }
+
+        Livro novaOferta = new Livro(posicao, valor, quantidade);
+
+        Array.Resize(ref livroDeOfertas, livroDeOfertas.Length + 1);
+        livroDeOfertas[livroDeOfertas.Length - 1] = novaOferta;
+
+        return livroDeOfertas;
+    }
+
+    static Livro[] ModificarOferta(Livro[] livroDeOfertas, int posicao, double valor, int quantidade)
+    {
+        int index = Array.FindIndex(livroDeOfertas, oferta => oferta.Posicao == posicao);
+
+        if (index != -1)
+        {
+            if (valor != 0)
+                livroDeOfertas[index].Valor = valor;
+            if (quantidade != 0)
+                livroDeOfertas[index].Quantidade = quantidade;
+        }
+
+        return livroDeOfertas;
+    }
+
+    static Livro[] RemoverOferta(Livro[] livroDeOfertas, int posicao)
+    {
+        if (posicao != 1 && posicao > livroDeOfertas[livroDeOfertas.Length - 1].Posicao) 
+        {
+            return livroDeOfertas;
+        }
+
+        Livro[] novoVetor = new Livro[livroDeOfertas.Length - 1];
+
+        if (posicao == 1) 
+        {
+            for (int i = 1; i <= livroDeOfertas.Length - 1; i++)
+            {
+                Livro novaOferta = new Livro(i, livroDeOfertas[i].Valor, livroDeOfertas[i].Quantidade);
+                novoVetor[i - 1] = novaOferta;
+            }
+        }
+        else 
+        {
+            for (int i = 0; i <= posicao - 2; i++)
+            {
+                Livro novaOferta = new Livro(i, livroDeOfertas[i].Valor, livroDeOfertas[i].Quantidade);
+                novoVetor[i] = novaOferta;
+            }
+            for (int i = posicao; i < posicao; i++)
+            {
+                Livro novaOferta = new Livro(i, livroDeOfertas[i].Valor, livroDeOfertas[i].Quantidade);
+                novoVetor[i] = novaOferta;
+            }
+        }
+
+        livroDeOfertas = novoVetor;
+
+        return livroDeOfertas;
+    }
+
+
+    static void ImprimirLivroDeOfertas(Livro[] livroDeOfertas)
+    {
+        Console.WriteLine();
+
+        for (int i = 0; i <= livroDeOfertas.Length - 1; i++)
+        {
+            if (i < livroDeOfertas.Length - 1)
+            {
+                Console.WriteLine($"{livroDeOfertas[i].Posicao},{livroDeOfertas[i].Valor},{livroDeOfertas[i].Quantidade}");
+            }
+            else
+            {
+                Console.Write($"{livroDeOfertas[i].Posicao},{livroDeOfertas[i].Valor},{livroDeOfertas[i].Quantidade}\\\n");
+            }
+        }
+
+
+    }
+}
+
+class Livro
+{
+    public int Posicao { get; set; }
+    public double Valor { get; set; }
+    public int Quantidade { get; set; }
+
+    public Livro(int posicao, double valor, int quantidade)
+    {
+        Posicao = posicao;
+        Valor = valor;
+        Quantidade = quantidade;
+    }
+}
---
+++ menor_distancia_array/menor_distancia_array/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MenorDistanciaArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { -1, 5, 11, 20, 35, 50, 65, 70, 85, 100 };
+            int[] array2 = { 26, 6, 14, 29, 37, 41, 55, 75, 80, 90 };
+
+            int menorDistancia = EncontrarMenorDistancia(array1, array2, out int num1, out int num2);
+
+            Console.WriteLine($"A menor distância é {menorDistancia}! Entre os números: {num1} do array 1 e {num2} do array 2.");
+        }
+
+        static int EncontrarMenorDistancia(int[] array1, int[] array2, out int num1, out int num2)
+        {
+            int menorDistancia = int.MaxValue;
+            num1 = 0;
+            num2 = 0;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    int distancia = Math.Abs(array1[i] - array2[j]);
+
+                    if (distancia < menorDistancia)
+                    {
+                        menorDistancia = distancia;
+                        num1 = array1[i];
+                        num2 = array2[j];
+                    }
+                }
+            }
+
+            return menorDistancia;
+        }
+    }
+}
